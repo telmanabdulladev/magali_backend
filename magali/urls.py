59d312f82c09e3a90714b8e53452a12bd13ab5e9
Telman Abdulla <telmanabdulladev@gmail.com>
2023-06-22 14:57:19 +0400

    collectstatic

--- magali/urls.py
+++ magali/urls.py
@@ -21,10 +21,13 @@ from django.conf.urls.static import static
 from django.conf import settings
 from magali_app import views
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index,name="index"),
     path('magali/',include('magali_app.urls')),
+    
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
 urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
